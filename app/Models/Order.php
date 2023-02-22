92bf1eaf29ccc51adb782598d9c0bb361a2a6c0f
R4GUDA <66072295+R4GUDA@users.noreply.github.com>
2023-02-22 17:59:09 +0300

    Rework models

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'delivery_date',
+        'phone',
+        'email',
+        'address',
+        'secret',
+        'delivery_date'
+    ];
+
+    protected $dateFormat = "Y-m-d";
+
+    public function products() {
+        return $this->belongsToMany(Product::class, OrderProduct::class)->withPivot('amount');
+    }
 }
