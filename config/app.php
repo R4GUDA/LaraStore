496f491fceb8bcea95f29783ae4e35c22cf595c0
R4GUDA <66072295+R4GUDA@users.noreply.github.com>
2023-02-22 17:59:30 +0300

    Add datatables

--- config/app.php
+++ config/app.php
@@ -185,7 +185,7 @@ return [
         /*
          * Package Service Providers...
          */
-        Yajra\DataTables\DataTablesServiceProvider::class,
+        yajra\DataTables\DataTablesServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -211,7 +211,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
-        'DataTables' => Yajra\DataTables\Facades\DataTables::class,
+        'DataTables' => yajra\DataTables\Facades\DataTables::class,
     ])->toArray(),
 
 ];
