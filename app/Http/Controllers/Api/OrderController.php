286ea4d56bb6c864d8aff45d20cb8963c204ff92
R4GUDA <66072295+R4GUDA@users.noreply.github.com>
2023-02-22 18:02:33 +0300

    Create order controllers

---
+++ app/Http/Controllers/Api/OrderController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Order\CreateOrderRequest;
+use App\Http\Requests\Order\OrderRequest;
+use App\Http\Requests\Order\UpdateOrderRequest;
+use App\Http\Resources\Order\MinPriceResource;
+use App\Http\Resources\Order\OrderCreatedResource;
+use App\Http\Resources\Order\OrderDeletedResource;
+use App\Http\Resources\Order\OrderResource;
+use App\Http\Resources\Order\OrderUpdatedResource;
+use App\Http\Resources\Order\ShowOrderResource;
+use App\Models\Order;
+use App\Services\OrderService;
+use Illuminate\Http\Request;
+use Yajra\DataTables\Facades\DataTables;
+
+class OrderController extends Controller
+{
+
+    public function index(OrderRequest $request) {
+        $orders = OrderService::index($request);
+
+        return OrderResource::collection($orders);
+    }
+
+    public function store(CreateOrderRequest $request) {
+        if (OrderService::calcTotal($request) < 3000) return MinPriceResource::make([]);
+
+        $orders = OrderService::store($request);
+
+        return OrderCreatedResource::make($orders);
+    }
+
+    public function show(Order $order) {
+        $total = 0;
+
+        foreach ($order->products as $product) {
+            $total += $product->pivot->amount * $product->price;
+        }
+
+        $order->total = round($total,2);
+
+        return ShowOrderResource::make($order);
+    }
+
+    public function update(UpdateOrderRequest $request, Order $order) {
+        $order = OrderService::update($request, $order);
+
+        return OrderUpdatedResource::make($order);
+    }
+
+    public function destroy(Order $order) {
+        $order->delete();
+
+        return OrderDeletedResource::make([]);
+    }
+
+
+    public function getOrders(Request $request) {
+        $data = Order::latest()->get();
+        return Datatables::of($data)
+            ->addIndexColumn()
+            ->addColumn('action', function($row){
+                $actionBtn = '<a href="javascript:void(0)" class="edit btn btn-success btn-sm">Edit</a> <a href="javascript:void(0)" class="delete btn btn-danger btn-sm">Delete</a>';
+                return $actionBtn;
+            })
+            ->rawColumns(['action'])
+            ->make(true);
+    }
+}
---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Order\OrderRequest;
+use App\Http\Requests\Order\UpdateOrderRequest;
+use App\Models\Order;
+use App\Services\OrderService;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    public function index(OrderRequest $request) {
+        $orders = OrderService::index($request);
+
+        return view('orders', [
+            'orders' => $orders
+        ]);
+    }
+
+    public function show(Order $order) {
+        $total = 0;
+
+        foreach ($order->products as $product) {
+            $total += $product->pivot->amount * $product->price;
+        }
+
+        return view('order', [
+            'order' => $order,
+            'total' => $total
+        ]);
+    }
+
+    public function update(UpdateOrderRequest $request, Order $order) {
+        OrderService::update($request, $order);
+
+        return back();
+    }
+
+    public function destroy(Order $order) {
+        $order->delete();
+
+        return redirect(route('order.index'));
+    }
+
+    public function find(Request $request) {
+        $order = Order::whereSecret($request->secret)->first();
+
+        $total = 0;
+
+        foreach ($order->products as $product) {
+            $total += $product->pivot->amount * $product->price;
+        }
+
+        return view('find', [
+            'order' => $order,
+            'total' => $total
+        ]);
+    }
+}
